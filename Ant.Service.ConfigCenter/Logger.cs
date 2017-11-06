678eadcaadfdbce7040bef30cde75df5eefb3688
sqb <shenqiangbin@163.com>
2017-11-06 15:15:00 +0800

    add log4net

---
+++ Ant.Service.ConfigCenter/Logger.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"Log4net.config", Watch = true)]
+namespace Ant.Service.ConfigCenter
+{
+    public static class Logger
+    {
+        public static void Log(string msg)
+        {
+            ILog _log = LogManager.GetLogger("FileLogger");
+            _log.Error(msg);
+        }
+
+    }
+
+    //public class SqlLogger
+    //{
+    //    public static void Log(string msg)
+    //    {
+    //        ILog _log = LogManager.GetLogger("SysLogger");
+    //        _log.Error(msg);
+    //    }
+
+    //    public static void Log(object msg)
+    //    {
+    //        ILog _log = LogManager.GetLogger("SysLogger");
+    //        _log.Error(msg);
+    //    }
+
+    //    public static void Log(Exception ex)
+    //    {
+    //        ILog _log = LogManager.GetLogger("SysLogger");
+    //        _log.Error(ex.Message + ex.StackTrace);
+    //    }
+    //}
+
+}
