81f0a83f2d3db41810553d2fc188f59004cad540
sqb <shenqiangbin@163.com>
2017-11-06 15:14:44 +0800

    添加log4net

--- Ant.Service.ConfigCenter/Program.cs
+++ Ant.Service.ConfigCenter/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace Ant.Service.ConfigCenter
 {
@@ -10,6 +12,27 @@ namespace Ant.Service.ConfigCenter
     {
         static void Main(string[] args)
         {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            HostFactory.Run(x =>
+            {
+                x.Service<Service>(s =>
+                {
+                    s.ConstructUsing(name => new Service());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc =>
+                    {
+                        tc.Stop();
+                        //_container.Dispose();
+                    });
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("配置中心，提供基础配置服务");
+                x.SetDisplayName("Ant程序的配置中心");
+                x.SetServiceName("Ant.Service.ConfigCenter");
+                //x.UseAutofacContainer(_container);
+            });
         }
     }
 }
---
+++ Ant.Service.ConfigCenter/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ant.Service.ConfigCenter
+{
+    public class Service
+    {
+        public void Start()
+        {
+            Logger.Log("服务开启");
+        }
+
+        public void Stop()
+        {
+            Logger.Log("服务停止");
+        }
+    }
+}
